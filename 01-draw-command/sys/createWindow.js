b26828598f961c4b005a62e52d1d7ab90c9628d9
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-11 16:07:08 +0100

    01-draw-command createWindow plask polyfils

--- 01-draw-command/sys/createWindow.js
+++ 01-draw-command/sys/createWindow.js
@@ -5,6 +5,7 @@ var createBrowserWindow = require('./createBrowserWindow');
 function createWindow(opts) {
   opts.settings = opts.settings || {};
   opts.settings.type = '3d';
+  opts.settings.highdpi = 1;
 
   //wait for all resources to load
   if (opts.resources) {
@@ -12,6 +13,10 @@ function createWindow(opts) {
     opts._draw = opts.draw;
     opts.draw = function() {};
     opts.init = function() {
+      //Add missing webgl context props
+      this.gl.drawingBufferWidth = this.width;
+      this.gl.drawingBufferHeight = this.height;
+
       Promise.all(R.values(opts.resources)).then(function(resources) {
         opts.draw = function() {
           if (opts.update) {
