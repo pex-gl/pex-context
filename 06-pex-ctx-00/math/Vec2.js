02c00e4194dc52cd9c820554bdc0db5cf335ad4a
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-18 13:33:29 +0100

    06-pex-ctx-00 added create to Vec2

--- 06-pex-ctx-00/math/Vec2.js
+++ 06-pex-ctx-00/math/Vec2.js
@@ -1,9 +1,14 @@
+function create() {
+    return [0, 0];
+}
+
 function equals(a,b) {
     return a[0] == b[0] &&
            a[1] == b[1];
 }
 
 var Vec2 = {
+    create : create,
     equals : equals
 };
 
