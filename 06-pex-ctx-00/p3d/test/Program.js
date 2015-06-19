2a653b6834ea858c884e7b773432af808e2bd085
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-19 10:17:14 +0100

    06-pex-ctx-00 Program uniform test

--- 06-pex-ctx-00/p3d/test/Program.js
+++ 06-pex-ctx-00/p3d/test/Program.js
@@ -9,8 +9,10 @@ void main() { \
 ';
 
 var FRAG_SRC = '\
+uniform vec4 uColor; \
 void main() { \
   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \
+  gl_FragColor = uColor; \
 } \
 ';
 
@@ -20,8 +22,10 @@ Window.create({
   },
   init: function() {
     var ctx = this.getContext();
+    //TODO: new Program(ctx, VERT_SRC, FRAG_SRC) -> ctx.createProgram(VERT_SRC, FRAG_SRC)
     var program = new Program(ctx, VERT_SRC, FRAG_SRC);
     program.bind();
+    program.setUniform('uColor', [1, 0, 0, 1]);
     program.unbind();
   },
   update: function() {
@@ -29,5 +33,9 @@ Window.create({
   },
   draw: function() {
     var ctx = this.getContext();
+    //ctx.setClearColor([1, 0, 0, 1]);
+    ctx.setClearColor(1, 0, 0, 1);
+
+    ctx.clear(ctx.COLOR_BIT);
   }
 })
