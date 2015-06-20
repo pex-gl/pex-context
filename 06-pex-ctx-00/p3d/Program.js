4c2ebfbaccdb8b7c716a10e2796a73ac41968db6
Henryk Wollik <olafyoorsen@hotmail.com>
2015-06-20 21:06:26 +0200

    06-pex-ctx Program added attribute loc bindings check

--- 06-pex-ctx-00/p3d/Program.js
+++ 06-pex-ctx-00/p3d/Program.js
@@ -1,30 +1,33 @@
 var DefaultAttributeLocationBinding = require('./ProgramAttributeLocationBinding');
 
 //TODO: this is true in 99% of cases, might be implementation specific
-var MaxVertexAttributes = 16;
+var NUM_VERTEX_ATTRIBUTES_MAX = 16;
 
-function Program(context, vertSrc, fragSrc, attributeLocationBinding) {
+var STR_ERROR_UNIFORM_UNDEFINED = 'Uniform "%s" is not defined.';
+var STR_ERROR_WRONG_NUM_ARGS = 'Wrong number of arguments.';
+var STR_ERROR_INVALID_UNIFORM_TYPE = 'Invalid uniform type "%s".';
+var STR_ERROR_ATTRIBUTE_BINDING_UNDEFINED = 'Attribute "%s" is not present in program.';
+
+function Program(context, vertSrc, fragSrc, attributeLocationBinding){
     var gl = this._gl = context.getGL();
 
     //TODO: creating program once like Pex or on every load like Foam
     //If we don't do this in init then bind() can be invalid and it should throw
-    this._handle = gl.createProgram();
+    this._handle     = gl.createProgram();
     this._attributes = {};
-    this._uniforms = {};
-    if (vertSrc) {
+    this._uniforms   = {};
+    if(vertSrc){
         this.update(vertSrc, fragSrc, attributeLocationBinding);
     }
 }
 
-Program.prototype.bind = function() {
-    var gl = this._gl;
-    gl.useProgram(this._handle);
-}
+Program.prototype.getHandle = function(){
+    return this._handle;
+};
 
-Program.prototype.unbind = function() {
-    var gl = this._gl;
-    //TODO: How Program.unbind works? We no longer have bind()/unbind() at all? And we do ctx.bindProgram(program);
-    gl.useProgram(null);
+Program.prototype._bindInternal = function(){
+
+    this._gl.useProgram(this._handle);
 };
 
 /**
@@ -33,7 +36,7 @@ Program.prototype.unbind = function() {
  * @param  {String} [fragSrc]               - frag shader source
  * @param  {String} [attributeLocationBinding] - attribute locations map { 0: 'aPositon', 1: 'aNormal', 2: 'aColor' }
  */
-Program.prototype.update = function(vertSrc, fragSrc, attributeLocationBinding) {
+Program.prototype.update = function(vertSrc, fragSrc, attributeLocationBinding){
     var gl = this._gl;
     var program = this._handle;
 
@@ -43,7 +46,7 @@ Program.prototype.update = function(vertSrc, fragSrc, attributeLocationBinding) 
     gl.attachShader(program, vertShader);
     gl.attachShader(program, fragShader);
 
-    for(var location = 0; location < MaxVertexAttributes; location++) {
+    for(var location = 0; location < NUM_VERTEX_ATTRIBUTES_MAX; location++){
         var attributeName = (attributeLocationBinding && attributeLocationBinding[location]) || DefaultAttributeLocationBinding[location];
         gl.bindAttribLocation(program, location, attributeName);
     }
@@ -55,16 +58,23 @@ Program.prototype.update = function(vertSrc, fragSrc, attributeLocationBinding) 
     gl.deleteShader(vertShader);
     gl.deleteShader(fragShader);
 
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    if(!gl.getProgramParameter(program, gl.LINK_STATUS)){
         throw new Error('PROGRAM: ' + gl.getProgramInfoLog(program));
     }
 
     this._updateUniforms();
     this._updateAttributes();
-}
 
-Program.prototype._updateUniforms = function() {
-    var gl          = this._gl;
+    for(var location in attributeLocationBinding){
+        var attributeName = attributeLocationBinding[location];
+        if(this._attributes[attributeName] === undefined){
+            throw new Error(STR_ERROR_ATTRIBUTE_BINDING_UNDEFINED.replace('%s', attributeName));
+        }
+    }
+};
+
+Program.prototype._updateUniforms = function(){
+    var gl = this._gl;
     var program     = this._handle;
     var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
     var uniforms    = this._uniforms = {};
@@ -73,92 +83,91 @@ Program.prototype._updateUniforms = function() {
         info = gl.getActiveUniform(program, i);
         name = info.name;
         uniforms[name] = {
-            type     : info.type,
+            type : info.type,
             location : gl.getUniformLocation(program, name)
         };
     }
-}
+};
 
-Program.prototype._updateAttributes = function() {
-  var gl            = this._gl;
-  var program       = this._handle;
-  var numAttributes = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
-  var attributes    = this._attributes = {};
-
-  for(var i = 0, info, name; i < numAttributes; ++i){
-      info = gl.getActiveAttrib(program, i);
-      name = info.name;
-      attributes[name] = {
-          type     : info.type,
-          location : gl.getAttribLocation(program, name)
-      }
-  }
-  return attributes;
-}
-
-Program.prototype._compileVertexSource = function(vertSrc) {
+Program.prototype._updateAttributes = function(){
     var gl = this._gl;
+    var program       = this._handle;
+    var numAttributes = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+    var attributes    = this._attributes = {};
 
+    for(var i = 0, info, name; i < numAttributes; ++i){
+        info = gl.getActiveAttrib(program, i);
+        name = info.name;
+        attributes[name] = {
+            type : info.type,
+            location : gl.getAttribLocation(program, name)
+        }
+    }
+};
+
+Program.prototype._compileVertexSource = function(vertSrc){
+    var gl = this._gl;
     var vertShader = gl.createShader(gl.VERTEX_SHADER);
+
     gl.shaderSource(vertShader, vertSrc + '\n');
     gl.compileShader(vertShader);
-    if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
-        throw new Error('VERTEX: ' + gl.getShaderInfoLog(this.vertShader));
+    if(!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)){
+        throw new Error('VERTEX: ' + gl.getShaderInfoLog(vertShader));
     }
     return vertShader;
 };
 
-Program.prototype._compileFragmentSource = function(fragSrc) {
+Program.prototype._compileFragmentSource = function(fragSrc){
     var gl = this._gl;
     var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragShader, fragSrc + '\n');
     gl.compileShader(fragShader);
-    if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+    if(!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)){
         throw new Error('FRAGMENT: ' + gl.getShaderInfoLog(fragShader));
     }
     return fragShader;
 };
 
-Program.prototype.setUniform = function(name, args) {
+Program.prototype.setUniform = function(name, args){
     var uniform = this._uniforms[name];
-    if (uniform === undefined){
-        throw new Error(STR_ERROR_UNIFORM_UNDEFINED.replace('%s',name));
+    if(uniform === undefined){
+        throw new Error(STR_ERROR_UNIFORM_UNDEFINED.replace('%s', name));
     }
     var type     = uniform.type;
     var location = uniform.location;
     var numArgs  = arguments.length - 1;
     var gl       = this._gl;
 
-    switch (type){
+    switch(type){
         case gl.INT:
         case gl.BOOL:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniform1i(location,arguments[1]);
+            gl.uniform1i(location, arguments[1]);
             break;
         case gl.SAMPLER_2D:
         case gl.SAMPLER_CUBE:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniform1i(location,arguments[1]);
+            gl.uniform1i(location, arguments[1]);
             //but could also be texture
             break;
         case gl.FLOAT:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniform1f(location,arguments[1]);
+            gl.uniform1f(location, arguments[1]);
             break;
         case gl.FLOAT_VEC2:
             if(numArgs != 1 && numArgs != 2){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
             if(numArgs == 1){
-                gl.uniform2fv(location,arguments[1]);
-            } else {
-                gl.uniform2f(location,arguments[1],arguments[2]);
+                gl.uniform2fv(location, arguments[1]);
+            }else{
+                gl.uniform2f(location, arguments[1], arguments[2]);
             }
             break;
         case gl.FLOAT_VEC3:
@@ -166,9 +175,9 @@ Program.prototype.setUniform = function(name, args) {
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
             if(numArgs == 1){
-                gl.uniform3fv(location,arguments[1]);
-            } else {
-                gl.uniform3f(location,arguments[1],arguments[2],arguments[3]);
+                gl.uniform3fv(location, arguments[1]);
+            }else{
+                gl.uniform3f(location, arguments[1], arguments[2], arguments[3]);
             }
             break;
         case gl.FLOAT_VEC4:
@@ -176,37 +185,37 @@ Program.prototype.setUniform = function(name, args) {
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
             if(numArgs == 1){
-                gl.uniform4fv(location,arguments[1]);
-            } else {
-                gl.uniform4f(location,arguments[1],arguments[2],arguments[3],arguments[4]);
+                gl.uniform4fv(location, arguments[1]);
+            }else{
+                gl.uniform4f(location, arguments[1], arguments[2], arguments[3], arguments[4]);
             }
             break;
         case gl.FLOAT_MAT2:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniformMatrix2fv(location,false,arguments[1]);
+            gl.uniformMatrix2fv(location, false, arguments[1]);
             break;
         case gl.FLOAT_MAT3:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniformMatrix3fv(location,false,arguments[1]);
+            gl.uniformMatrix3fv(location, false, arguments[1]);
             break;
         case gl.FLOAT_MAT4:
             if(numArgs != 1){
                 throw new Error(STR_ERROR_WRONG_NUM_ARGS);
             }
-            gl.uniformMatrix4fv(location,false,arguments[1]);
+            gl.uniformMatrix4fv(location, false, arguments[1]);
             break;
         default :
-            throw new Error(STR_ERROR_INVALID_UNIFORM_TYPE.replace('%s',type));
+            throw new Error(STR_ERROR_INVALID_UNIFORM_TYPE.replace('%s', type));
             break;
     }
-}
+};
 
 Program.prototype.dispose = function(){
-    if(!this._handle) {
+    if(!this._handle){
         return this;
     }
     this._gl.deleteProgram(this._handle);
