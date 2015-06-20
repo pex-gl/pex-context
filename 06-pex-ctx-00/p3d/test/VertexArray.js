9b1d819d21e4f1194b48c1cdfdf8993b6d1afa5f
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-20 23:24:19 +0100

    06-pex-ctx-00 working VertexArray example

--- 06-pex-ctx-00/p3d/test/VertexArray.js
+++ 06-pex-ctx-00/p3d/test/VertexArray.js
@@ -1,23 +1,32 @@
+var Platform = require('../../sys/Platform');
 var Window = require('../../sys/Window');
 var Program = require('../Program');
 
 var VERT_SRC = '\
-attribute vec2 position; \
+attribute vec2 aPosition; \
+attribute vec4 aColor; \
+varying vec4 vColor; \
 void main() { \
-    gl_Position = vec4(position, 0.0, 1.0); \
+    gl_Position = vec4(aPosition, 0.0, 1.0); \
+    vColor = aColor; \
 } \
 ';
 
 var FRAG_SRC = '\
-uniform vec4 uColor; \
+varying vec4 vColor; \
 void main() { \
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); \
-    gl_FragColor = uColor; \
+    gl_FragColor = vColor; \
 } \
 ';
 
+if (Platform.isBrowser) {
+    FRAG_SRC = 'precision highp float; \n' + FRAG_SRC;
+}
+
 Window.create({
     settings: {
+        width: 800,
+        height: 600,
         type: '3d'
     },
     init: function() {
@@ -42,16 +51,16 @@ Window.create({
             0, 0, 1, 1
         ]);
 
-        var indexBuffer = ctx.createBuffer(ctx.ELEMENT_BUFFER, indices, ctx.STATIC_DRAW);
+        var indexBuffer = ctx.createBuffer(ctx.ELEMENT_ARRAY_BUFFER, indices, ctx.STATIC_DRAW);
         var positionBuffer = ctx.createBuffer(ctx.ARRAY_BUFFER, vertices, ctx.DYNAMIC_DRAW);
         var colorBuffer = ctx.createBuffer(ctx.ARRAY_BUFFER, colors, ctx.STATIC_DRAW);
         var attributes = [
-            { buffer: positionBuffer, location: ctx.ATTRIB_POSITION, size: 3, type: ctx.FLOAT, offset: 0, stride: 12, divisor: 0 },
-            { buffer: colorBuffer, location: ctx.ATTRIB_COLOR, size: 3, type: ctx.FLOAT, offset: 0, stride: 12, divisor: 0 }
+            { buffer: positionBuffer, location: ctx.ATTRIB_POSITION, size: 2, offset: 0, stride: 8 },
+            { buffer: colorBuffer, location: ctx.ATTRIB_COLOR, size: 4, offset: 0, stride: 16 }
         ];
         var vao = ctx.createVertexArray(attributes, indexBuffer);
-        ctx.bindVertexArray(vao);
-        ctx.draw(ctx.TRIANGLES, 0, vertices.length/3);
+
+        this.vao   = vao;
     },
     update: function() {
 
@@ -60,7 +69,9 @@ Window.create({
         var ctx = this.getContext();
         //ctx.setClearColor([1, 0, 0, 1]);
         ctx.setClearColor(0.2, 0.2, 0.2, 1);
+        ctx.clear(ctx.COLOR_BIT | ctx.DEPTH_BIT);
 
-        ctx.clear(ctx.COLOR_BIT);
+        ctx.bindVertexArray(this.vao);
+        ctx.draw(ctx.TRIANGLES, 0, this.vao.getIndexBuffer().getLength());
     }
 })
