b81404aabde1b52c3fe703e2731ec4caecb59b79
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-26 17:09:55 +0100

    06-pex-ctx-00 Moved pixels update to Texture2D.update

--- 06-pex-ctx-00/p3d/Texture2D.js
+++ 06-pex-ctx-00/p3d/Texture2D.js
@@ -1,13 +1,13 @@
 var Platform = require('../sys/Platform');
 
 function Texture2D(ctx, data, width, height, options) {
-    var gl = this._gl = ctx.getGL();
+    this._ctx        = ctx;
+    var gl           = this._gl = ctx.getGL();
     this._handle     = gl.createTexture();
     this._target     = gl.TEXTURE_2D;
     this._width      = width;
     this._height     = height;
 
-    //TODO: implement options / sampler object
     var internalFormat  = (options && options.format) || gl.RGBA;
     var format          = (options && options.format) || gl.RGBA;
     var repeat          = (options && options.repeat) || false;
@@ -15,13 +15,8 @@ function Texture2D(ctx, data, width, height, options) {
     var flip            = (options && options.flip  ) || false;
     var lod             = 0;
 
-    //FIXME: use context to bind texture
-    this._bindInternal();
-    //FIXME: this doesn't matter as long as we keep the currently bound texture
-    gl.activeTexture(gl.TEXTURE0);
-
-    //TODO: Is this needed? We are pushing data below anyway?
-    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, width, height, 0, format, dataType, null);
+    //TODO: Should we push stack here?
+    ctx.bindTexture(this, 0);
 
     var wrapS = repeat ? gl.REPEAT : gl.CLAMP_TO_EDGE;
     var wrapT = repeat ? gl.REPEAT : gl.CLAMP_TO_EDGE;
@@ -33,6 +28,24 @@ function Texture2D(ctx, data, width, height, options) {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, wrapS);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, wrapT);
 
+    this.update(data, width, height, options);
+}
+
+Texture2D.prototype.update = function(data, width, height, options) {
+    var ctx = this._ctx;
+    var gl  = this._gl;
+
+    //TODO: Should we push stack here?
+    ctx.bindTexture(this, 0);
+
+    //TODO: this should remember settings from constructor
+    var internalFormat  = (options && options.format) || gl.RGBA;
+    var format          = (options && options.format) || gl.RGBA;
+    var repeat          = (options && options.repeat) || false;
+    var dataType        = (options && options.type  ) || gl.UNSIGNED_BYTE;
+    var flip            = (options && options.flip  ) || false;
+    var lod             = (options && options.lod   ) || 0;
+
     if (Platform.isPlask) {
         if (flip) {
           gl.texImage2DSkCanvas(this._target, lod, data);
@@ -43,11 +56,15 @@ function Texture2D(ctx, data, width, height, options) {
     }
     else if (Platform.isBrowser) {
         gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, flip);
-        gl.texImage2D(gl.TEXTURE_2D, lod, internalFormat, format, dataType, data);
+        //Image, ImageData or Canvas
+        if (data.width && data.height) {
+            gl.texImage2D(gl.TEXTURE_2D, lod, internalFormat, format, dataType, data);
+        }
+        //Array buffer
+        else {
+            gl.texImage2D(gl.TEXTURE_2D, lod, internalFormat, width, height, 0, format, dataType, data);
+        }
     }
-
-    //FIXME: use context to unbind texture
-    gl.bindTexture(gl.TEXTURE_2D, null);
 }
 
 Texture2D.prototype._bindInternal = function() {
