57718d1ec87a220ac635a3cce58fe49a11be0819
Henryk Wollik <olafyoorsen@hotmail.com>
2015-06-30 17:29:12 +0200

    06-pex-ctx-00 ProgramUniform added NORMAL_MATRIX, INVERSE_VIEW_MATRIX

--- 06-pex-ctx-00/p3d/ProgramUniform.js
+++ 06-pex-ctx-00/p3d/ProgramUniform.js
@@ -1,7 +1,9 @@
 var ProgramUniform = {
-    PROJECTION_MATRIX : 'uProjectionMatrix',
-    VIEW_MATRIX       : 'uViewMatrix',
-    MODEL_MATRIX      : 'uModelMatrix'
+    PROJECTION_MATRIX    : 'uProjectionMatrix',
+    VIEW_MATRIX          : 'uViewMatrix',
+    MODEL_MATRIX         : 'uModelMatrix',
+    NORMAL_MATRIX        : 'uNormalMatrix',
+    INVERSE_VIEW_MATRIX  : 'uInverseViewMatrix'
 };
 
 module.exports = ProgramUniform;
