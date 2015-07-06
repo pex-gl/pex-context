ffe122b663474105715e003cd3c54b032f453e23
Marcin Ignac <marcin.ignac@gmail.com>
2015-07-06 12:10:14 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    Buffer: require('./Buffer'),
+    Context: require('./Context'),
+    Framebuffer: require('./Framebuffer'),
+    Mesh: require('./Mesh'),
+    Program: require('./Program'),
+    Texture2D: require('./Texture2D'),
+    TextureCube: require('./TextureCube'),
+    VertexArray: require('./VertexArray')
+};
