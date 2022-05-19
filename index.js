0b2e8d13c37e289096d107fe7449a2834bdb768e
Damien Seguin <dmnsgn@gmail.com>
2022-05-19 11:06:27 +0100

    refactor: move polyfill into utils

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ import {
   compareFBOAttachments,
   enableVertexData,
   NAMESPACE,
+  polyfill,
 } from "./utils.js";
 import { addEnums } from "./types.js";
 
@@ -86,106 +87,6 @@ function createContext(options = {}) {
     ),
   };
 
-  if (!gl.HALF_FLOAT) {
-    const ext = gl.getExtension("OES_texture_half_float");
-    if (ext) gl.HALF_FLOAT = ext.HALF_FLOAT_OES;
-  }
-
-  if (!gl.createVertexArray) {
-    const ext = gl.getExtension("OES_vertex_array_object");
-    if (!ext) {
-      gl.createVertexArray = function () {
-        throw new Error("OES_vertex_array_object not supported");
-      };
-    } else {
-      gl.createVertexArray = ext.createVertexArrayOES.bind(ext);
-      gl.bindVertexArray = ext.bindVertexArrayOES.bind(ext);
-      capabilities.vertexArrayObject = true;
-    }
-  } else {
-    capabilities.vertexArrayObject = true;
-  }
-
-  if (!capabilities.disjointTimerQuery) {
-    gl.TIME_ELAPSED ||= "TIME_ELAPSED";
-    gl.GPU_DISJOINT ||= "GPU_DISJOINT";
-    gl.QUERY_RESULT ||= "QUERY_RESULT";
-    gl.QUERY_RESULT_AVAILABLE ||= "QUERY_RESULT_AVAILABLE";
-    gl.createQuery ||= () => ({});
-    gl.deleteQuery ||= () => {};
-    gl.beginQuery ||= () => {};
-    gl.endQuery ||= () => {};
-    gl.getQueryParameter = (q, param) => {
-      if (param === gl.QUERY_RESULT_AVAILABLE) return true;
-      if (param === gl.QUERY_RESULT) return 0;
-      return undefined;
-    };
-    gl.getQueryObject = gl.getQueryParameter;
-  } else {
-    const extDTQ = capabilities.isWebGL2
-      ? gl.getExtension("EXT_disjoint_timer_query_webgl2")
-      : gl.getExtension("EXT_disjoint_timer_query");
-    gl.TIME_ELAPSED = extDTQ.TIME_ELAPSED_EXT;
-    gl.GPU_DISJOINT = extDTQ.GPU_DISJOINT_EXT;
-    gl.QUERY_RESULT ||= extDTQ.QUERY_RESULT_EXT;
-    gl.QUERY_RESULT_AVAILABLE ||= extDTQ.QUERY_RESULT_AVAILABLE_EXT;
-    gl.createQuery ||= extDTQ.createQueryEXT.bind(extDTQ);
-    gl.deleteQuery ||= extDTQ.deleteQueryEXT.bind(extDTQ);
-    gl.beginQuery ||= extDTQ.beginQueryEXT.bind(extDTQ);
-    gl.endQuery ||= extDTQ.endQueryEXT.bind(extDTQ);
-    gl.getQueryParameter ||= extDTQ.getQueryObjectEXT.bind(extDTQ);
-  }
-
-  if (!gl.drawElementsInstanced) {
-    const ext = gl.getExtension("ANGLE_instanced_arrays");
-    if (!ext) {
-      // TODO: this._caps[CAPS_INSTANCED_ARRAYS] = false;
-      gl.drawElementsInstanced = () => {
-        throw new Error(
-          "gl.drawElementsInstanced not available. ANGLE_instanced_arrays not supported"
-        );
-      };
-      gl.drawArraysInstanced = () => {
-        throw new Error(
-          "gl.drawArraysInstanced not available. ANGLE_instanced_arrays not supported"
-        );
-      };
-      gl.vertexAttribDivisor = () => {
-        throw new Error(
-          "gl.vertexAttribDivisor not available. ANGLE_instanced_arrays not supported"
-        );
-      };
-    } else {
-      // TODO: this._caps[CAPS_INSTANCED_ARRAYS] = true;
-      gl.drawElementsInstanced = ext.drawElementsInstancedANGLE.bind(ext);
-      gl.drawArraysInstanced = ext.drawArraysInstancedANGLE.bind(ext);
-      gl.vertexAttribDivisor = ext.vertexAttribDivisorANGLE.bind(ext);
-      capabilities.instancedArrays = true;
-      capabilities.instancing = true; // TODO: deprecate
-    }
-  } else {
-    capabilities.instancedArrays = true;
-    capabilities.instancing = true; // TODO: deprecate
-  }
-
-  if (!gl.drawBuffers) {
-    const ext = gl.getExtension("WEBGL_draw_buffers");
-    if (!ext) {
-      gl.drawBuffers = () => {
-        throw new Error("WEBGL_draw_buffers not supported");
-      };
-    } else {
-      gl.drawBuffers = ext.drawBuffersWEBGL.bind(ext);
-      capabilities.maxColorAttachments = gl.getParameter(
-        ext.MAX_COLOR_ATTACHMENTS_WEBGL
-      );
-    }
-  } else {
-    capabilities.maxColorAttachments = gl.getParameter(
-      gl.MAX_COLOR_ATTACHMENTS
-    );
-  }
-
   const ctx = {
     gl,
     capabilities,
@@ -197,6 +98,7 @@ function createContext(options = {}) {
     },
   };
 
+  polyfill(ctx);
   addEnums(ctx);
 
   const defaultState = {
