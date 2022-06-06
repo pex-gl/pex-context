7bef258deafe4cda5cb6d37d481e9df77e8f6d5b
Damien Seguin <dmnsgn@gmail.com>
2022-06-06 17:05:23 +0100

    docs: add try/catch for importShim

--- examples/utils.js
+++ examples/utils.js
@@ -9,6 +9,7 @@ export const es300Fragment = (shader, size = 1) => /* glsl */ `#version 300 es
 precision highp float;
 #define varying in
 #define texture2D texture
+#define textureCube texture
 #define gl_FragData FragData
 #define gl_FragColor gl_FragData[0]
 out vec4 FragData[${size}];
